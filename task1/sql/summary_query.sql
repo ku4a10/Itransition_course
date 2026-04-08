2b1f3557a63dd6e1dbd7f89105b8e32cb006846c
ku4a10 <blakc7055@gmail.com>
2026-04-08 12:01:05 +0500

    Fix summary_query.sql

--- task1/sql/summary_query.sql
+++ task1/sql/summary_query.sql
@@ -2,7 +2,7 @@ SELECT year,
     COUNT(*) AS book_count,
     ROUND(AVG(
         CASE
-            WHEN currency = '€' THEN price * 1.2
+            WHEN currency = 'EUR' THEN price * 1.2
             ELSE price
         END), 2) AS avg_price_in_usd           
 FROM books
